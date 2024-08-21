bc959d9e1a8e51c1b9436a1f984ef17fa0b57fe3
Amaan63 <amaansayed63@gmail.com>
2024-08-21 21:55:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// Used For Customizing Tailwind
+
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = { 
+  // Saari HTML files ko watch Karo
+  // Js file React Mai use karenge
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
